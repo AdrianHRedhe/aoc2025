f00381bf16ae0ce237aa99a11eb714a70b71393c
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2026-04-07 17:41:06 +0200

    feat(Util): add possibility to read from test file as well when developing

--- Util.scala
+++ Util.scala
@@ -1,3 +1,4 @@
-def readInput(day: Int): List[String] =
+def readInput(day: Int, test: Boolean = false): List[String] =
   val padded = f"day$day%02d"
-  scala.io.Source.fromFile(s"inputs/$padded.txt").getLines().toList
+  val suffix = if test then "_test" else ""
+  scala.io.Source.fromFile(s"inputs/$padded$suffix.txt").getLines().toList
